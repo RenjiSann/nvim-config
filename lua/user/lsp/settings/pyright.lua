8bebbf08f69c0d9e33c8f24d1fca70ab775a4eca
Dorian 'renji' Péron <dorianperon.i@gmail.com>
2023-03-10 01:23:14 +0100

    Update tab size, ad clangd

--- lua/user/lsp/settings/pyright.lua
+++ lua/user/lsp/settings/pyright.lua
@@ -6,4 +6,13 @@ return {
       },
     },
   },
+  before_init = function(params, config)
+    local Path = require "plenary.path"
+    local venv = Path:new((config.root_dir:gsub("/", Path.path.sep)), ".venv")
+    if venv:joinpath("bin"):is_dir() then
+      config.settings.python.pythonPath = tostring(venv:joinpath("bin", "python"))
+    else
+      config.settings.python.pythonPath = tostring(venv:joinpath("Scripts", "python.exe"))
+    end
+  end
 }
